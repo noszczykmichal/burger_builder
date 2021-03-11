a517352b5e5e80419a9cde0f769b02ce69d3325c
Michał Noszczyk <noszczykmichal@wp.pl>
2021-03-11 23:17:52 +0100

    added the action creators for logging out the user + modified reducer to updated the store

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -1,4 +1,4 @@
-import * as actions from '../actions/actionTypes';
+import * as actionTypes from '../actions/actionTypes';
 import { updateObject } from '../utility';
 
 const initialState = {
@@ -26,13 +26,21 @@ const authFail = (state, action) => {
         error: action.error,
         loading: false
     });
-}
+};
+
+const authLogout = (state, action) => {
+    return updateObject(state, {
+        token: null,
+        userId: null
+    });
+};
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case actions.AUTH_START: return authStart(state, action);
-        case actions.AUTH_SUCCESS: return authSuccess(state, action);
-        case actions.AUTH_FAIL: return authFail(state, action);
+        case actionTypes.AUTH_START: return authStart(state, action);
+        case actionTypes.AUTH_SUCCESS: return authSuccess(state, action);
+        case actionTypes.AUTH_FAIL: return authFail(state, action);
+        case actionTypes.AUTH_LOGOUT: return authLogout(state, action);
         default: return state;
     }
 };
