02b3d6730c96db3f87a700cae766a668816666ee
Michał Noszczyk <noszczykmichal@wp.pl>
2021-03-23 21:25:51 +0100

    fixing the redirect to the frontpage  + minor visual fixes

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -7,21 +7,21 @@ import Aux from '../../../hoc/Auxiliary/Auxiliary';
 
 const sideDrawer = (props) => {
 
-    let attachedClasses=[classes.SideDrawer, classes.Close];
+    let attachedClasses = [classes.SideDrawer, classes.Close];
 
-    if(props.open){
-        attachedClasses=[classes.SideDrawer, classes.Open];
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
     }
 
     return (
         <Aux>
-            <Backdrop show={props.open} clicked={props.closed}/>
-            <div className={attachedClasses.join(' ')}>
+            <Backdrop show={props.open} clicked={props.closed} />
+            <div className={attachedClasses.join(' ')} onClick={props.closed}>
                 <div className={classes.Logo}>
                     <Logo />
                 </div>
                 <nav>
-                    <NavigationItems isAuthenticated={props.isAuth}/>
+                    <NavigationItems isAuthenticated={props.isAuth} />
                 </nav>
             </div>
         </Aux>
